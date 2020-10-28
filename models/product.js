b4c4abfc72a0e59e5b682e8daaeffd5af7a64488
WayneJr <blairu92@gmail.com>
2020-10-28 14:10:29 -0400

    minor changes

--- models/product.js
+++ models/product.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
     name: String,
-    description: String,
-    image: String,
     price: String,
     category: {
         id: {
